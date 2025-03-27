9b54c9c99dc6d85c786d3d44caef7914138efeca
Ahamed <ahamed13041998@gmail.com>
2025-03-27 18:12:41 +0530

    added backend setting

--- backend.tf
+++ backend.tf
@@ -4,5 +4,6 @@ terraform {
     storage_account_name = "demostorage3000"
     container_name       = "tf-blob"
     key                  = "terraform.tfstate"
+    use_oidc              = true
   }
 }
