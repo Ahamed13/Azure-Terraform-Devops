6b9b517516abd018d085f69b969084028537dd1b
Ahamed <ahamed13041998@gmail.com>
2025-03-27 19:34:38 +0530

    Revert "Update 06-vnet-input-variables.tf"
    This reverts commit e93d22bac12953d8981724d0b6c88815fb4210e0.

--- 06-vnet-input-variables.tf
+++ 06-vnet-input-variables.tf
@@ -72,7 +72,7 @@ variable "bastion_subnet_address" {
   
 }
 
-/*variable "subscription_id" {}
+variable "subscription_id" {}
 variable "tenant_id" {}
 variable "client_id" {}
-variable "client_secret" {}*/
+variable "client_secret" {}
